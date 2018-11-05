77f23c8cf3d2addac94df3ba8445fa0018b4b767
Fábián János <fabian.janos93@gmail.com>
2018-11-05 11:29:59 +0100

    init

---
+++ src/main/java/com/codecool/mrguinneapig/wotd/wotd/WotdApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.mrguinneapig.wotd.wotd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WotdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WotdApplication.class, args);
+    }
+}
