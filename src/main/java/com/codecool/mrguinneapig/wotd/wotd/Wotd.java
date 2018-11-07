1a11fffc92afe76076bd7e6317c451cb65091746
Fábián János <fabian.janos93@gmail.com>
2018-11-07 09:55:57 +0100

    file read fix for heroku

--- src/main/java/com/codecool/mrguinneapig/wotd/wotd/Wotd.java
+++ src/main/java/com/codecool/mrguinneapig/wotd/wotd/Wotd.java
@@ -13,6 +13,8 @@ public class Wotd {
 
     public static final String STATIC_WORDS_TXT = "static/words.txt";
     String wotd;
+    List<String> lines;
+
 
     public Wotd() throws IOException {
         this.wotd = WordOfTheDay();
@@ -26,17 +28,13 @@ public class Wotd {
         return wotd;
     }
 
-    public static int getSeededRandom(int max) {
-        Date date = new Date();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
-        String seed = sdf.format(date);
-        Random seededRandom = new Random(Integer.parseInt(seed.replace("/", "")));
-        int random = Math.abs(seededRandom.nextInt(max));
-        return random;
+    private void fillLines() {
+        lines.addAll(Arrays.asList());
     }
 
     public String WordOfTheDay() throws IOException {
         List<String> lines = new LinkedList<>();
+        
         ClassLoader cl = this.getClass().getClassLoader();
         InputStream inputStream = cl.getResourceAsStream(STATIC_WORDS_TXT);
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
@@ -49,9 +47,12 @@ public class Wotd {
         return lines.get(getSeededRandom(lines.size()));
     }
 
-    List<String> lines;
-
-    private void fillLines() {
-        lines.addAll(Arrays.asList());
+    public static int getSeededRandom(int max) {
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+        String seed = sdf.format(date);
+        Random seededRandom = new Random(Integer.parseInt(seed.replace("/", "")));
+        int random = Math.abs(seededRandom.nextInt(max));
+        return random;
     }
 }
